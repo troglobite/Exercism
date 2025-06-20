739fc73f83380e14a4cd975228f24ca64a511a16
Brian Parry <brian.parry@acima.com>
2025-06-20 09:54:28 -0600

    HASKELL LUHN - FUCKING SOLVED IT

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -9,7 +9,7 @@ testLuhn = (isValid "4539 3195 0343 6467") == True
 isValid :: String -> Bool
 isValid n
         | length n <= 1 = False
-        | otherwise = checkSum . doubled . integered . filtered $ n
+        | otherwise     = checkSum . everyOtherDoubled . reverse . integered . filtered $ n
 
 
 -- strip spaces from string and non-digits from strings
@@ -21,24 +21,10 @@ integered :: [ Char ] -> [ Int ]
 integered s = digitToInt <$> s
 
 -- double every other digit starting from the right
-doubled :: [ Int ] -> [ Int ]
-doubled is =
-  -- snd $ mapAccumL (\a x -> if even a then (increment a, x) else (increment a, subNine $ double x)) 1 is
-  case is of
-    [] -> []
-    [_x] -> []
-    [x, y] -> [subNine $ double x, y]
-    (x:y:z) -> subNine (double x) : y : doubled z
-
-
---doubled' =
---  scanr1 (everyOther double)
-
-
-everyOther ::( a -> a ) -> [ a ] -> [ a ]
-everyOther _ [] = []
-everyOther _ [_x] = []
-everyOther f (x:y:xs) = x : f y : everyOther f xs
+everyOtherDoubled :: [ Int ] -> [ Int ]
+everyOtherDoubled []      = []
+everyOtherDoubled [x]     = [subNine . double $ x]
+everyOtherDoubled (x:y:z) = x : (subNine . double $ y) : everyOtherDoubled z
 
 
 double :: Int -> Int
@@ -50,7 +36,6 @@ subNine i = if i > 9 then i - 9 else i
 
 -- sum all digits, if divisible by 10 then valid
 checkSum :: [ Int ] -> Bool
+checkSum [0] = False
 checkSum is = rem (sum is) 10 == 0
 
-
--- Test case "059" is wrong as its not valid according to the luhn algorithm.
