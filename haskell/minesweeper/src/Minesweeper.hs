59cb625875aa53e301695be2a0402ac35fc76b77
Brian Parry <brian.parry@acima.com>
2026-04-17 08:57:38 -0600

    HASKELL-MINESWEEPER: Figured out makeCoordinates

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -16,8 +16,8 @@ testBoard = ["***", "***", "***"]
 
 mineCount :: [String] -> Int -> Int -> Maybe Int
 mineCount board x y
-  | x < 0 || x >= (length $ board !! 1) = Nothing
-  | y < 0 || y >= (length $ board) = Nothing
+  | x < 0 || x >= length (board !! 1) = Nothing
+  | y < 0 || y >= length board = Nothing
   | otherwise = Just $ length $ catMaybes [l, r, t, b]
   where l = valueAt board (x - 1) y
         r = valueAt board (x + 1) y -- board !? y !? (x + 1)
@@ -26,17 +26,19 @@ mineCount board x y
 
 valueAt :: [String] -> Int -> Int -> Maybe Char
 valueAt board x y
-  | x < 0 || x >= (length $ board !! 1) = Nothing
-  | y < 0 || y >= (length $ board) = Nothing
+  | x < 0 || x >= length (board !! 1) = Nothing
+  | y < 0 || y >= length board = Nothing
   | otherwise = Just $ board !! y !! x
 
 
 -- TODO: Map a board over buildRow
 --       buildRow take the y position you are building out
 --       and the row of xs
-makeCoordinates :: [[x]] -> Int -> Int -> [(x, Int, Int)]
-makeCoordinates xs x y = undefined
+makeCoordinates :: [[x]] -> [[(Int, Int, x)]]
+makeCoordinates xs = buildRow xs <$> [0..(length xs - 1)]
 
 
-buildRow :: [x] -> Int -> [(Int, Int, x)]
-buildRow xs y = zip3 [0..(length xs)] (take (length xs) $ repeat y) (xs !! y)
+buildRow :: [[x]] -> Int -> [(Int, Int, x)]
+buildRow board y = zip3 [0..(length board - 1)] (replicate (length board) y) (board !! y)
+
+testMakeCoordinate = makeCoordinates testBoard == [[(0,0,'*'),(1,0,'*'),(2,0,'*')],[(0,1,'*'),(1,1,'*'),(2,1,'*')],[(0,2,'*'),(1,2,'*'),(2,2,'*')]]
